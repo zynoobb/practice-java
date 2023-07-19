80c5dce20fbfd05a245ad0f2ca7e021865e0ce18
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-07-19 20:34:46 +0900

    Feat : section3 static block과 main()

---
+++ src/section3/Hello2.java
@@ -0,0 +1,18 @@
+package section3;
+
+public class Hello2 {
+    static int i = 0;
+    static {
+        i = 500;
+        System.out.println("static block"); // 원래는 static 필드를 초기화함
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+
+// javac Hello2.java // 컴파일
+// java Hello2 // 실행
+
+// static 블록 이용 시 main함수보다 먼저 실행되는 함수를 만들 수 있음
